bc676a6a72c908fd213bc72ce9637b2c0fd3fafc
TheWintersShadow <elijah.winter3@gmail.com>
2024-10-17 13:18:01 -0400

    Initial Commit

---
+++ Simple Mind/Simple_MindApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_MindApp.swift
+//  Simple Mind
+//
+//  Created by Eli on 10/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Simple_MindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Simple MindTests/Simple_MindTests.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_MindTests.swift
+//  Simple MindTests
+//
+//  Created by Eli on 10/17/24.
+//
+
+import Testing
+@testable import Simple_Mind
+
+struct Simple_MindTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
